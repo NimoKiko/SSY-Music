5f1f034169d8e18dd808077835c28eb7d474288e
hyf <973528232@qq.com>
2024-06-06 17:08:21 +0800

    build frontend

---
+++ ssymusic/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import tabbar from "./components/tabbar/tabbar.vue";
+</script>
+
+<template>
+  <div class="container">
+    <tabbar class="tabbar"></tabbar>
+  </div>
+</template>
+
+<style scoped lang="less">
+.container {
+  height: 100vh;
+  border: 1px solid red;
+  position: relative;
+
+  .tabbar{
+    position: absolute;
+    bottom: 0;
+  }
+}
+</style>
---
+++ ssymusic/src/components/tabbar/tabItem.vue
@@ -0,0 +1,28 @@
+<!--
+  功能：功能描述
+  作者：黄逸凡
+  邮箱：973528232@qq.com
+  时间：2024年06月06日 17:05:37
+  版本：v1.0
+  修改记录：
+  修改内容：
+  修改人员：
+  修改时间：
+-->
+<template>
+<div id="tab-item">
+  <img class="tab-icon" src="" alt="">
+  <span></span>
+</div>
+</template>
+
+<script setup lang='ts' name='tabItem'>
+</script> 
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!--使用了scoped属性之后，父组件的style样式将不会渗透到子组件中，-->
+<!--然而子组件的根节点元素会同时被设置了scoped的父css样式和设置了scoped的子css样式影响，-->
+<!--这么设计的目的是父组件可以对子组件根元素进行布局。-->
+<style scoped>
+
+</style>
